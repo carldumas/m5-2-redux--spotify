aacf71a4383e33d65817f07d724e65e08f3dffa0
carldumas <dumas.carl@gmail.com>
2020-09-18 18:30:50 -0400

    M2-5: Redux Spotify - Exercise 5 - Routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,36 @@
 // Libraries
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom';
+// Components
+import ArtistRoute from '../ArtistRoute';
 // Styles
+import styled from 'styled-components';
 import GlobalStyles from '../GlobalStyles';
+
+const DEFAULT_ARTIST_ID = '6qqNVTkY8uBg9cP3Jd7DAH';
 
 const App = () => {
     return (
         <Router>
+            <Wrapper>
+                <Switch>
+                    <Route path="/artists/:id">
+                        <ArtistRoute />
+                    </Route>
+                    <Redirect to={`/artists/${DEFAULT_ARTIST_ID}`} />
+                </Switch>
+            </Wrapper>
             TODO
             <GlobalStyles />
         </Router>
     );
 };
+
+const Wrapper = styled.div``;
 
 export default App;
---
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ArtistRoute extends Component {
+    render() {
+        return <div>Artist page</div>;
+    }
+}
+
+export default ArtistRoute;
